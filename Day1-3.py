8c09509aa3c2c23c6e31871a6139c4b68a252b6c
stephanie19950405 <69131939+stephanie19950405@users.noreply.github.com>
2020-08-03 11:56:19 +0800

    Add files via upload

---
+++ Day1-3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 11:24:27 2020
+
+@author: AE401
+"""
+
+r=input("輸入圓的半徑")
+ansC= float(r)*2*3.14
+print("圓周長是",ansC)
+ansA=float(r)*float(r)*3.14
+print("圓面積是",ansA)
+
+
