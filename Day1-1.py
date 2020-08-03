2e4476b4619d0f9f5f07b31a013612b9f4c25148
stephanie19950405 <69131939+stephanie19950405@users.noreply.github.com>
2020-08-03 11:16:19 +0800

    Add files via upload

---
+++ Day1-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:07:37 2020
+
+@author: AE401
+"""
+
+print("I'm Eric \nHe is 'cool'")
+print("I'm Eric \nHe is \"cool\"")
---
+++ Day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:27:56 2020
+
+@author: AE401
+"""
+
+name=input("請問你的名字?")
+print(name +",Hi")
